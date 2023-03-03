feb03c6e20135743be8009237df994d9c8d70a67
AmeMeida <ame.pistache@gmail.com>
2023-03-03 07:58:20 -0300

    formulários, post e get

---
+++ formMedia.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="calcMedia.php" method="post">
+        <label for="n1">Nota 1</label>
+        <input type="number" name="nota1" id="n1">
+
+        <br>
+
+        <label for="n2">Nota 2</label>
+        <input type="number" name="nota2" id="n2">
+
+        <br><br>
+
+        <input type="submit" value="Calcular">
+    </form>
+</body>
+</html>
