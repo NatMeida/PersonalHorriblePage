656a9b15bc0c6a257301ac40717c4e767cdd1815
AmeMeida <ame.pistache@gmail.com>
2023-05-17 09:59:19 -0300

    a

---
+++ pdo/database.php
@@ -0,0 +1,14 @@
+<?php
+    try {
+        require("../env.php");
+
+        $db = "mysql:host=$host;dbname=$database;charset=utf8";
+        $pdo = new PDO($db, $username, $password);
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "Conectado ao servidor";
+    } catch (PDOException $err) {
+        echo "ERRO DE CONEXÃO:  $err\n<br>";
+    }
+?>
---
+++ sessions/cookie.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<?php?>
+
+<body>
+    <h1>Cookies</h1>
+
+    <p>Cookies são informações que são armazenadas no computador do seu cliente.</p>
+
+    <p>Você pode pedir para o navegador do seu usuário armazenar cookies como o tema favorito para o site (claro ou escuro), informações de login, preferências e histórico de acesso.</p>
+
+    <p>Toda a vez que o usuário fizer uma requisição para o seu endereço, os cookies serão reenviados. Dessa forma, seu servidor poderá receber as informações e adequar a página ou continuar de onde o usuário parou.</p>
+</body>
+</html>
+
+pelo menos ela vai mostrar um ORM ao inves de torturar a gente com mysql na mao que nem a pripri
+e assim pelo menos nao tem risco do usuario "joao'; DROP DATABASE" ser cadastrado
